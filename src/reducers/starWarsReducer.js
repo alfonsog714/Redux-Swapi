57fb704c66f948ac73fefb539593be596b2b583c
Alfonso <alfonso.garcia2324@gmail.com>
2019-05-15 13:32:24 -0700

    App now fetches character data if successful, else it displays the error. For some reason, I can not get this to work on Google Chrome, but the application works just fine on Mozilla Firefox

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,34 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING_START, FETCHING_FAILED, FETCHING_SUCCESS } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  isFetching: false,
+  error: ""
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
+    case FETCHING_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: ""
+      };
+
+    case FETCHING_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: "",
+        characters: action.payload
+      };
+
+    case FETCHING_FAILED:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
     default:
       return state;
   }
